637c5e4b8e3582fa280bbc5b34eead51c3b11ace
shubro07 <136472052+shubro007@users.noreply.github.com>
2026-01-10 03:20:02 +0530

    added js

---
+++ java.js
@@ -0,0 +1,11 @@
+var btn1 = document.getElementById("btn1");
+btn1.addEventListener("click", function() {
+    navigator.vibrate(200);
+});
+
+var btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", function() {
+    setTimeout( ()=>{
+        navigator.vibrate([50, 200, 50, 200, 50, 200, 50]);
+    }, 5000)
+});
